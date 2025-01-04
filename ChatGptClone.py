d472b058352d0d9a02e921379ef916a75ddc6d25
Kornu Sai Govinda Rao <kornusaigovinda@gmail.com>
2025-01-05 00:32:08 +0530

    ChatGPT Clone
    Create a Chatgpt Clone by using Python Programming Openai and Streamlit Modules

---
+++ ChatGptClone.py
@@ -0,0 +1,41 @@
+from openai import OpenAI
+import time
+def chat_with_gpt(prompt):
+
+    client = OpenAI(
+        api_key=""
+    )
+
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        store=True,
+        messages=[
+            {"role": "user", "content": prompt}
+        ]
+    )
+    text = ""
+    response = completion.choices[0].message.content.strip()
+    for i in response.split(" "):
+        yield i + " "
+        time.sleep(0.06)
+
+
+if __name__ == "__main__":
+    import streamlit as sl
+    sl.set_page_config(
+        page_title="ChatGPT Clone",
+        page_icon="img_2.png"
+    )
+    sl.markdown("<h1 style='text-align:center';>ChatGPT</h1>", unsafe_allow_html=True)
+    question = sl.chat_input(placeholder="Enter Question")
+    if question:
+        sl.write_stream(chat_with_gpt(question))
+
+
+
+
+
+
+
+
+
